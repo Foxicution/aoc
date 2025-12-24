a8600908059d8f14bbc3a449321e35efe15bade2
Foxicution <foxicution@gmail.com>
2025-12-24 23:04:24 +0200

    solve 2015 day 1

---
+++ 2015/01/rust/src/main.rs
@@ -0,0 +1,54 @@
+fn part1(input: &str) -> i32 {
+    input
+        .trim()
+        .bytes()
+        .map(|ch| if ch == b'(' { 1 } else { -1 })
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    let mut floor = 0;
+
+    for (i, ch) in input.bytes().enumerate() {
+        floor += if ch == b'(' { 1 } else { -1 };
+
+        if floor == -1 {
+            return i + 1;
+        }
+    }
+    0
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("(())", 0)]
+    #[case("()()", 0)]
+    #[case("(((", 3)]
+    #[case("(()(()(", 3)]
+    #[case("))(((((", 3)]
+    #[case("())", -1)]
+    #[case("))(", -1)]
+    #[case(")))", -3)]
+    #[case(")())())", -3)]
+    fn test_part1(#[case] input: &str, #[case] expected: i32) {
+        assert_eq!(part1(input), expected)
+    }
+
+    #[rstest]
+    #[case(")", 1)]
+    #[case("()())", 5)]
+    fn test_part2(#[case] input: &str, #[case] expected: usize) {
+        assert_eq!(part2(input), expected)
+    }
+}
