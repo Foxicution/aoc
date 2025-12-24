bef89a57cc69a59a1e4113fe6ad3e57e49af4205
Foxicution <foxicution@gmail.com>
2025-12-24 23:45:43 +0200

    solve 2015 day 2

---
+++ 2015/02/rust/src/main.rs
@@ -0,0 +1,63 @@
+fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split('x');
+            let (l, w, h): (u32, u32, u32) = (
+                parts.next().unwrap().parse().unwrap(),
+                parts.next().unwrap().parse().unwrap(),
+                parts.next().unwrap().parse().unwrap(),
+            );
+
+            let (w1, w2, w3) = (l * w, w * h, h * l);
+            let min = w1.min(w2).min(w3);
+            2 * (w1 + w2 + w3) + min
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split('x').map(|s| s.parse::<u32>().unwrap());
+            let mut dims = [
+                parts.next().unwrap(),
+                parts.next().unwrap(),
+                parts.next().unwrap(),
+            ];
+
+            dims.sort();
+            let (l, w, h) = (dims[0], dims[1], dims[2]);
+
+            2 * (l + w) + l * w * h
+        })
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("2x3x4", 58)]
+    #[case("1x1x10", 43)]
+    fn test_part1(#[case] input: &str, #[case] expected: u32) {
+        assert_eq!(part1(input), expected)
+    }
+
+    #[rstest]
+    #[case("2x3x4", 34)]
+    #[case("1x1x10", 14)]
+    fn test_part2(#[case] input: &str, #[case] expected: u32) {
+        assert_eq!(part2(input), expected)
+    }
+}
